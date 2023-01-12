d5dfeea18229f5bcaca84eac796841bd4081b35b
brooklynnlafferty <brooklynn.lafferty@yahoo.com>
2023-01-12 15:15:49 -0500

    <brooklynnlafferty> Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -2,12 +2,15 @@
 library (swirl)
 swirl()
 Brooklynn
+1
+1
+1
 5+7
 x<- 5+7
 x
 y<- x-3
 y
-z <- c(1.1,9,3.14)
+z<- c(1.1,9,3.14)
 ?c
 z
 c(z,555,z)
@@ -16,10 +19,10 @@ my_sqrt<- sqrt(z-1)
 1
 my_sqrt
 my_div<- z/my_sqrt
-1
+3
 my_div
 c(1,2,3,4)+c(0,10)
 c(1,2,3,4)+c(0,10,100)
 z*2+1000
 my_div
-2
+1
