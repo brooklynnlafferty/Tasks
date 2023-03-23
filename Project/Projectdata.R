6641fcef8ccc7c51f3818868eae21ce7f8628f69
brooklynnlafferty <brooklynn.lafferty@yahoo.com>
2023-03-23 14:28:28 -0400

    Brooklynn Lafferty Project Data

--- Project/Projectdata.R
+++ Project/Projectdata.R
@@ -12,8 +12,10 @@ chisq.test(data.table)
 #for a 95% CI with df=6 the theoretical value is 1.635; the calculated
 #Chi-Squared analysis value is 1833.4 so we reject the null hypothesis. 
 #This shows that the results were NOT due to random chance. 
+pdf("Project graph.pdf")
 slices<- c(19.8,6.5,62.8,10.9)
 lbls<- c("Black","White","Brown","Wood")
 pct<-(slices/sum(slices)*100)
 lbls<- paste(lbls,pct)
 pie(slices,labels=lbls,col=rainbow(length(lbls)),main="Percentages of Colors Landed on by Biting Flies")
+dev.off()
