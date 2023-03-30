6c4496125ae1cbaf0faacc303ed9bef21fbf63c4
brooklynnlafferty <brooklynn.lafferty@yahoo.com>
2023-03-30 15:50:22 -0400

    Task 08

---
+++ Task_08/task08.r
@@ -0,0 +1,66 @@
+setwd('~/Desktop/Evolution/Tasks/Task_08')
+library(phytools)
+trees<- c()
+births<- c()
+Fractions<- c()
+random<- c()
+random2<- c()
+treelist<- c()
+for(i in 1:100){
+births[i]<- runif(1)
+Fractions[i]<- runif(1)
+trees[[i]]<- pbtree(n=100,b=births[1],d=births[i]*Fractions[i])
+random[[i]]<- births[i]
+random2[[i]]<- (Fractions[i])
+treelist[[i]]<- mean(trees[[i]]$edge.length)
+}
+div_rate<- sapply(trees,function(x)length(x$tip.label))
+log_tips<- log(sapply(trees,function(x)length(x$tip.label)))
+library(ggplot2)
+ggplot(data=data.frame(div_rate,log_tips),aes(x=log_tips,y=div_rate))
+geom_point()
+xlab("Log Number of Tips") 
+ylab("Net Diversification vs. Log Number of Tips")  
+ggtitle("Net Diversification vs. Log Number of Tips")
+dev.off()
+speciation_rate<- sapply(births,function(x)x)
+avg_branchlength<- sapply(trees,function(x)mean(x$edge.length,na.rm=TRUE))
+pdf("plot1_08.pdf")
+library(ggplot2)
+ggplot(data=data.frame(speciation_rate,avg_branchlength),aes(x=speciation_rate,y=avg_branchlength))+
+geom_point()+
+xlab("Speciation Rate")+ 
+ylab("Average Branch Length")+  
+ggtitle("Speciation Rate vs. Average Branch Length")  
+dev.off()
+cor(speciation_rate,avg_branchlength)
+largest_tree<- trees[[which.max(sapply(trees,length))]]
+Tree<- largest_tree
+pdf("plot2_08.pdf")
+plot(Tree)
+dev.off()
+pdf("plot3_08.pdf")
+rates<- c()
+traits<- list()
+for(i in 1:100){
+rates[i]<- runif(1)  
+traits[[i]]<- fastBM(tree=Tree,sig2=rates[i])
+}
+mean_traits<- sapply(traits,mean)
+cor(mean_traits,rates)
+plot(rates,mean_traits,xlab="Rates",ylab="Mean Trait")
+dev.off()
+pdf("plot4_08.pdf")
+var_traits<- sapply(traits,var)
+cor(var_traits,rates)
+plot(rates,var_traits,xlab="Rates",ylab="Variance of Traits")
+dev.off()
+pdf("plot5_08.pdf")
+cor(traits[[1]],traits[[2]])
+traitMat<- cbind(traits[[1]],traits[[4]])
+plot(traitMat)
+dev.off()
+#Extra Credit
+pdf("plot6_08.pdf")
+phylomorphospace(Tree,traitMat,xlab="Trait 1",ylab="Trait 2")
+dev.off()
